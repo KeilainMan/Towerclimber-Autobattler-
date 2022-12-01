c62f6d5e264a3de3d470476a767ac73bc373b1c2
KeilainMan <109026727+KeilainMan@users.noreply.github.com>
2022-12-01 15:12:20 +0100

    Änderungen an Unitbase

--- Units/Unit_Stats.gd
+++ Units/Unit_Stats.gd
@@ -1,17 +1,20 @@
 extends Resource
 class_name Unit_Stats
 
-export var health:int = 0
-export var meele_damage:int = 0
-export var range_damage:int = 0
-export var ranged:bool = false
-export var attack_range:float = 0
-export var attack_speed:float = 0
-export var movement_speed:int = 0
-export var armor:int = 0 
-export var starting_mana:int = 0
-export var maximum_mana:int = 100
-export(Array, Vector2) var relative_positions = [Vector2.ZERO]
+# Character Info, hpts. für den Shop
+export var character_name: String = ""
+export var character_discription: String = ""
+
+# Character Stats
+export var health: int = 0
+export var damage: int = 0
+export var ranged: bool = false
+export var attack_range: int = 0
+export var attack_speed: float = 0
+export var movement_speed: int = 0
+export var armor: int = 0 
+export var starting_mana: int = 0
+export var maximum_mana: int = 100
 export(Array, String) var traits = []
 
-export var unit_value = 0
+
