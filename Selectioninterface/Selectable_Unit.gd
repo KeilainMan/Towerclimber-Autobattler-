d5a8c694ddef8e2cf7bdaaa473897f9115c1415f
KeilainMan <109026727+KeilainMan@users.noreply.github.com>
2022-12-04 17:14:27 +0100

    Vereinfachung des Selectioninterfaces
    Das Interface zum Platzieren einer Einheit kann nun durch einen Knopf (orangene Fläche) geöffnet werden und nach Auswahl eine Einheit wird es automatisch geschlossen.

--- Selectioninterface/Selectable_Unit.gd
+++ Selectioninterface/Selectable_Unit.gd
@@ -1,14 +1,15 @@
 extends TextureRect
 
 
-onready var button = get_node("Button")
-var hold_unit = null
+onready var button: Node = get_node("Button")
+var hold_unit: PackedScene = null
 
 
-func _ready():
+func _ready() -> void:
 	
 	
 	button.connect("pressed", self, "on_interface_unit_button_selected")
 	
 func on_interface_unit_button_selected():
 	Signals.emit_signal("wants_to_place_a_unit", hold_unit)
+	Signals.emit_signal("unit_choosen")
