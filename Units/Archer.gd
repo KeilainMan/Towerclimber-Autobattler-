14e1c7c7582a7814cffd8940279d230fc1865a65
KeilainMan <109026727+KeilainMan@users.noreply.github.com>
2022-12-27 10:14:39 +0100

    Archer Special Ability
    Einführung der SpezialFähigkeit für den Archer. AA Buff

--- Units/Archer.gd
+++ Units/Archer.gd
@@ -2,9 +2,18 @@ extends Unitbase
 
 onready var archer_resource = load("res://Units/Archer.tres")
 
+onready var ability_buff_timer: Node = get_node("AbilityBuffTimer")
+
+export var attack_speed_modifier: float = 5 # in Prozent
+export var attack_damage_modifier: float = 3 # in Prozent
+export var buff_time: int = 4
+var base_damage: int 
+var base_attack_speed: float
+
 func _ready():
 	set_character_stats(archer_resource)
 	prepare_unit()
+	_set_ability_buff_timer(buff_time)
 
 func perform_attack():
 	if _check_if_enemy_exists(focused_enemy_path):
@@ -13,3 +22,35 @@ func perform_attack():
 		elif !sprites.animation == "Attack_1":
 			_play_sprite_animation("Attack_1")
 	.perform_attack()
+
+
+func _set_ability_buff_timer(time) -> void:
+	ability_buff_timer.wait_time = time
+
+
+func perform_special_ability() -> void:
+	state = UnitState.CASTING_ABILITY
+	safe_pre_ability_stats()
+	ongoing_ability_running = true
+	damage *= attack_damage_modifier
+	attack_speed *= attack_speed_modifier
+	set_attackcooldowntimer(1/attack_speed)
+	ability_buff_timer.start()
+	state = UnitState.INACTIVE
+
+
+func safe_pre_ability_stats() -> void:
+	base_damage = damage
+	base_attack_speed = attack_speed
+
+
+func _on_AbilityBuffTimer_timeout() -> void:
+	_reset_to_base_stats()
+	ability_buff_timer.one_shot = true
+	ongoing_ability_running = false
+
+
+func _reset_to_base_stats() -> void:
+	damage = base_damage
+	attack_speed = base_attack_speed
+	set_attackcooldowntimer(1/attack_speed)
